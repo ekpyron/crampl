d851232e91cbb046881a130e56c868d1cc2be0d1
Daniel Kirchner <daniel@ekpyron.org>
2023-06-30 01:42:06 +0200

    Major multi key map improvements.

--- crampl/MultiKeyMap.h
+++ crampl/MultiKeyMap.h
@@ -8,6 +8,7 @@
  */
 #pragma once
 
+#include <algorithm>
 #include <optional>
 #include <utility>
 #include <ranges>
@@ -41,18 +42,6 @@ template<template<typename...> typename MapKind, typename Key, typename Value, t
 struct MultiKeyMap<MapKind, ComplexKey<Key, Tail...>, Value> {
     using type = MapKind<Key, Value, Tail...>;
 };
-
-template<typename T>
-struct MultiKeyMapValueType {
-    using type = T;
-};
-
-template<template<typename...> typename MapKind, typename Key, typename Value, typename... Tail>
-struct MultiKeyMapValueType<MapKind<Key, Value, Tail...>>: MultiKeyMapValueType<Value> {};
-
-template<typename T>
-using MultiKeyMapValueType_t = typename MultiKeyMapValueType<std::decay_t<T>>::type;
-
 }
 
 
@@ -61,21 +50,56 @@ using MultiKeyMap = typename detail::MultiKeyMap<MapKind, KeyTypes_AND_ValueType
 
 
 template<typename A, typename B, typename... C>
-std::optional<detail::MultiKeyMapValueType_t<A>> find(A&& map, B&& key, C&&... keys)
+auto find(A&& map, B&& key, C&&... keys)
 {
     if constexpr (sizeof...(keys) > 0) {
         auto it = map.find(key);
         if (it == map.end())
-            return {};
-        return find(it->second, keys...);
+            return std::optional<decltype(find(it->second, keys...))>{};
+        return std::make_optional(find(it->second, keys...));
     } else {
         auto it = map.find(key);
         if (map.find(key) == map.end())
-            return {};
-        return { it->second };
+            return std::optional<decltype(it->second)>{};
+        return std::make_optional(it->second);
     }
 }
 
+template<typename T>
+struct compose {
+    T lambda;
+};
+
+template<typename T>
+compose(T) -> compose<T>;
+
+template<typename Lambda1, typename Lambda2>
+auto operator+(compose<Lambda1> lhs, compose<Lambda2> rhs)
+{
+    return compose{[&]<typename... Args>(Args&&... args) mutable -> decltype(auto)
+    {
+        return std::apply(std::move(lhs.lambda), std::move(rhs.lambda)(std::forward<Args>(args)...));
+    }};
+}
+
+
+template<typename A,  typename... B>
+auto& emplace(A& map, std::piecewise_construct_t, B&&... keys) {
+    return [&]<auto... Ix>(std::index_sequence<Ix...>) -> decltype(auto) {
+        return std::get<0>((... + compose{[&](auto& inner, auto&& key, auto&&... args) mutable -> decltype(auto) {
+            static_cast<void>(Ix);
+            if constexpr (sizeof...(args) == 1) {
+                auto [it, _] = inner.emplace(std::piecewise_construct, static_cast<decltype(key)>(key), static_cast<decltype(args)>(args)...);
+                return std::make_tuple(std::ref(it->second), std::nullopt);
+            } else {
+                auto [it, _] = inner.emplace(std::piecewise_construct, static_cast<decltype(key)>(key), std::forward_as_tuple());
+                return std::make_tuple(std::ref(it->second), static_cast<decltype(args)>(args)...);
+            }
+        }}).lambda(map, std::forward<B>(keys)...));
+    }(std::make_index_sequence<sizeof...(keys) - 1>{});
+}
+
+/*
 template<typename A, typename B, typename... C>
 detail::MultiKeyMapValueType_t<A> &emplace(A&& map, std::piecewise_construct_t, B&& key, C&&... keys)
 {
@@ -87,5 +111,37 @@ detail::MultiKeyMapValueType_t<A> &emplace(A&& map, std::piecewise_construct_t, 
         return it->second;
     }
 }
+ */
+/*
+    template<typename A,  typename B1, typename... B>
+    auto& emplace(A& map, std::piecewise_construct_t, B1&& _key1, B&&... keys) {
+        return std::get<0>((... + compose{[&](auto& inner, auto&& key, auto&& value, auto&&... args) mutable -> decltype(auto) {
+            static_cast<void>(keys);
+            if constexpr (sizeof...(args) == 0) {
+                auto [it, _] = inner.emplace(std::piecewise_construct, static_cast<decltype(key)>(key), static_cast<decltype(value)>(value));
+                return std::make_tuple(std::ref(it->second));
+            } else {
+                auto [it, _] = inner.emplace(std::piecewise_construct, static_cast<decltype(key)>(key), std::forward_as_tuple());
+                return std::make_tuple(std::ref(it->second), static_cast<decltype(value)>(value), static_cast<decltype(args)>(args)...);
+            }
+        }}).lambda(map, std::forward<B1>(_key1), std::forward<B>(keys)...));
+    }
+*/
+
+/*
+template<typename A,  typename B1, typename... B>
+auto& emplace(A& map, std::piecewise_construct_t, B1&& _key1, B&&... keys) {
+    return std::get<0>((... + compose{[&]<typename Map, typename Key, typename Value, typename... Args>(Map& inner, Key&& key, Value&& value, Args&&... args) mutable -> decltype(auto) {
+        static_cast<void>(keys);
+        if constexpr (sizeof...(args) == 0) {
+            auto [it, _] = inner.emplace(std::piecewise_construct, std::forward<Key>(key), std::forward<Value>(value));
+            return std::make_tuple(std::ref(it->second));
+        } else {
+            auto [it, _] = inner.emplace(std::piecewise_construct, std::forward<Key>(key), std::forward_as_tuple());
+            return std::make_tuple(std::ref(it->second), std::forward<Value>(value), std::forward<Args>(args)...);
+        }
+    }}).lambda(map, std::forward<B1>(_key1), std::forward<B>(keys)...));
+}
+ */
 
 }
--- test/MultiKeyMap.cpp
+++ test/MultiKeyMap.cpp
@@ -22,14 +22,32 @@ TEST_CASE("MultiKeyMap") {
     REQUIRE(std::is_same_v<Xeample2, Example2>);
     REQUIRE(std::is_same_v<std::map<int, bool>, crampl::MultiKeyMap<std::map, int, bool>>);
 
-    crampl::MultiKeyMap<std::map, int, int, int> m3 { {21, { {42, 84} }} };
-    REQUIRE(crampl::find(m3, 21, 42) == std::optional<int>{84});
-
     std::map<std::string, int> m4 {{"foo", 3}};
     REQUIRE(!crampl::find(m4, "bar"));
     REQUIRE(crampl::find(m4, "foo"));
 
+    REQUIRE(crampl::emplace(m4, std::piecewise_construct, std::forward_as_tuple(std::string("x")), std::forward_as_tuple(42)) == 42);
+    int x = 21;
+    REQUIRE(m4["x"] == 42);
+    REQUIRE(crampl::emplace(m4, std::piecewise_construct, std::forward_as_tuple(std::string("y")), std::forward_as_tuple(x)) == 21);
+    REQUIRE(m4["y"] == 21);
 
+    std::map<std::string, std::map<int, float>> m3_2 {{"foo", {{3, 3.0f}}}};
+    crampl::emplace(m3_2, std::piecewise_construct, std::forward_as_tuple(std::string("x")), std::forward_as_tuple(7), std::forward_as_tuple(4.0f));
+
+
+    crampl::MultiKeyMap<std::map, int, int, int> m3 { {21, { {42, 84} }} };
+    REQUIRE(crampl::find(m3, 21, 42) == std::optional<int>{84});
+
+
+    {
+        crampl::MultiKeyMap<std::map, int, std::string, std::vector<char>> m4 { {21, { {"ABC", {{true},{false}}} }} };
+
+        auto& three = crampl::emplace(m4, std::piecewise_construct, std::forward_as_tuple(1), std::forward_as_tuple("a"), std::forward_as_tuple(std::vector<char>{{false},{true}}));
+        REQUIRE(three.size() == 2);
+        REQUIRE(three[0] == false);
+        REQUIRE(three[1] == true);
+    }
     {
         auto six = crampl::emplace(m4, std::piecewise_construct, std::forward_as_tuple("baz"),
                                    std::forward_as_tuple(6));
@@ -39,7 +57,6 @@ TEST_CASE("MultiKeyMap") {
                                        std::forward_as_tuple(7));
         REQUIRE(alsoSix == 6);
     }
-
     {
         auto& three = crampl::emplace(m3, std::piecewise_construct, std::forward_as_tuple(1), std::forward_as_tuple(2), std::forward_as_tuple(3));
         REQUIRE(three == 3);
